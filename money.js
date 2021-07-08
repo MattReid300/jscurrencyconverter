4c0717cc9533ce27a7c8190de5082b8e68d49a68
MattReid <matt.reid300@gmail.com>
2021-07-08 17:20:44 +0200

    Operations update

--- money.js
+++ money.js
@@ -1,37 +1,44 @@
 function convert() {
-  let thing = document.getElementById("listPicker");
-  let secondThing = document.getElementById("secondListPicker");
-  let listPicker = thing.options[thing.selectedIndex].value;
-  let secondListPicker = secondThing.options[secondThing.selectedIndex].value;
-  let result = 0;
+
   let amountInput = document.getElementById("inputNumber").value;
+  let base = document.getElementById("from").value;
+  let end = document.getElementById("to").value;
 
-  if(listPicker == secondListPicker) {
-    document.getElementById("answer").innerHTML = amountInput;
-  }
+  //   // EUROS
+  //   if (listPicker == "1" && secondListPicker == "2") {
+  //     apireq("EUR", "EUR", "POUND");
+  //   } else if (listPicker == "1" && secondListPicker == "3") {
+  //     apireq("USD", "GBP", "POUND");
+  //     // DOLLARS
+  //   } else if (listPicker == "2" && secondListPicker == "1") {
+  //     apireq("USD", "GBP", "POUND");
+  //   } else if (listPicker == "2" && secondListPicker == "3") {
+  //     apireq("USD", "GBP", "POUND");
+  //     // POUNDS
+  //   } else if (listPicker == "3" && secondListPicker == "1") {
+  //     apireq("USD", "GBP", "POUND");
+  //   } else if (listPicker == "3" && secondListPicker == "2") {
+  //     apireq("USD", "GBP", "POUND");
+  //   }
+  // }
 
-  // EUROS
-  if (listPicker == "1" && secondListPicker == "2") {
-    let result = (amountInput * 1.09).toFixed(2) + " dollars";
-    document.getElementById("answer").innerHTML = result;
-  } else if (listPicker == "1" && secondListPicker == "3") {
-    let result = (amountInput / 1.11).toFixed(2) + " pounds";
-    document.getElementById("answer").innerHTML = result;
-
-    // DOLLARS
-  } else if (listPicker == "2" && secondListPicker == "1") {
-    let result = (amountInput / 1.09).toFixed(2) + " euros";
-    document.getElementById("answer").innerHTML = result;
-  } else if (listPicker == "2" && secondListPicker == "3") {
-    let result = (amountInput / 1.26).toFixed(2) + " pounds";
-    document.getElementById("answer").innerHTML = result;
-
-    // POUNDS
-  } else if (listPicker == "3" && secondListPicker == "1") {
-    let result = (amountInput * 1.11).toFixed(2) + " euros";
-    document.getElementById("answer").innerHTML = result;
-  } else if (listPicker == "3" && secondListPicker == "2") {
-    let result = (amountInput * 1.26).toFixed(2) + " dollars";
-    document.getElementById("answer").innerHTML = result;
+  try {
+    return axios.get("https://api.exchangerate.host/latest", {
+      params: {
+        base
+      }
+    }).then(resp => {
+      const OPERATORS = {
+        'EURGBP': (a,b) => a * b,
+        'EURGBP': (a,b) => a * b,
+        'EURGBP': (a,b) => a * b,
+        'EURGBP': (a,b) => a * b,
+      }
+      let operand = resp.data.rates[end]
+      let result = OPERATORS[base+end] ? OPERATORS[base+end](amountInput, operand) : amountInput
+      document.getElementById("answer").innerHTML = result;
+    })
+  } catch (error) {
+    console.error(error)
   }
 }
